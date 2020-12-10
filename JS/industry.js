922a543bd9272289cb2d2523676be296228f4ea8
subo <suboxiu1011@icloud.com>
2020-12-10 16:12:55 +0800

    找錯

--- JS/industry.js
+++
@@ -1,9 +0,0 @@
-$(document).ready(function () {
-  let githubURL = new URL(location.href);
-  let params = githubURL.searchParams;
-  if (params.has('q')) {
-    var a = document.getElementById(params.get('q'));
-    $("#Internet-commerce").toggleClass("active show")
-    $(a).toggleClass('active show');
-  }
-});
