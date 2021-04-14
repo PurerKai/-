cdad3d4c690f80fe28e086b387f4e9a5b0aabc60
rindou0120 <raven258460@gmail.com>
2021-04-15 03:21:37 +0800

    0415代工版面第一次

---
+++ JS/retail.js
@@ -0,0 +1,16 @@
+$(".btn01").click(function () {
+    $('.edb1').addClass('hide');
+    $('.sec1').removeClass('hide');
+})
+$(".btn02").click(function () {
+    $('.edb2').addClass('hide');
+    $('.sec2').removeClass('hide');
+})
+$(".btn03").click(function () {
+    $('.edb3').addClass('hide');
+    $('.sec3').removeClass('hide');
+})
+
+$(".edbhide").click(function () {
+    $('.edb2').addClass('hide');
+})
